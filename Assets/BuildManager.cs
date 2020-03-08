df36d9a2c477ab055057d336f4f177fa95c2328c
Mr.X <dudarev.vlad@gmail.com>
2020-03-08 21:43:24 +0300

    Initial commit

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildManager : MonoBehaviour
+{
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+    void OnMouseEnter() {
+        transform.GetChild(0).GetComponent<Image>().color = Color.green;
+    }
+
+    void OnMouseExit() {
+        transform.GetChild(0).GetComponent<Image>().color = Color.white;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask whatCanBeclickedOn;
+    private NavMeshAgent myAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(myRay, out hitInfo, 100, whatCanBeclickedOn))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }        
+    }
+}
